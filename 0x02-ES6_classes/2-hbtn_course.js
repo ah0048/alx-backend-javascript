34923707b85f26dac49d4be51893013588753c85
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-04 17:42:51 +0300

    2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if ((typeof newName !== 'string') && !(newName instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if ((typeof newLength !== 'number') && !(newLength instanceof Number)) {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if ((typeof newStudents !== 'object') && !(newStudents instanceof Array) && newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
