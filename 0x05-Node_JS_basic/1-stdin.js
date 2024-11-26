26d5df86480be0d664e8b823df468d22f282839a
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-27 00:27:47 +0200

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf-8');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
-  process.stdin.end();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
