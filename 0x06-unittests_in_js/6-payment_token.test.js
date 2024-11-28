55d120a748374a78917f340c2d3f1f3b2e8cd72b
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-29 00:05:35 +0200

    6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return the correct value', () => new Promise((done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => done(err));
+  }));
+});
