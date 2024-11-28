aba7f975b1e69744df796888e1c77ff8571483b2
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-29 00:12:49 +0200

    6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,3 +1,4 @@
 module.exports = function getPaymentTokenFromAPI(success) {
-  if (success === true) return Promise.resolve({ data: 'Successful response from the API' });
-};
+    if (success === true)
+      return Promise.resolve({ data: 'Successful response from the API' });
+  };
