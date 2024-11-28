92e961c7ee9d39a156026150701d41b610f3ea91
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-28 22:22:56 +0200

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,22 @@
-import calculateNumber from './0-calcul.js';
-import { assert } from 'chai';
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-    it('should return 4', function() {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1.4, 3.5), 5);
-    });
-    it('should return 5', function() {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-    it('test for -ve numbers', function() {
-        assert.equal(calculateNumber(-1.5, 3.7), 3);
-        assert.equal(calculateNumber(1.5, -3.7), -2);
-    });
-    it('test for zeros ,-ve and +ve numbers', function() {
-        assert.equal(calculateNumber(0, 0), 0);
-        assert.equal(calculateNumber(-1, 1), 0);
-        assert.equal(calculateNumber(1, -1), 0);
-    });
+describe('calculateNumber', () => {
+  it('should return 4', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('test for -ve numbers', () => {
+    assert.equal(calculateNumber(-1.5, 3.7), 3);
+    assert.equal(calculateNumber(1.5, -3.7), -2);
+  });
+  it('test for zeros ,-ve and +ve numbers', () => {
+    assert.equal(calculateNumber(0, 0), 0);
+    assert.equal(calculateNumber(-1, 1), 0);
+    assert.equal(calculateNumber(1, -1), 0);
+  });
 });
