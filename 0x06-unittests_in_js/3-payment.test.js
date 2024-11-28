ae44a352c5c89d584bf1a2ea40ea1141ac48b244
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-28 23:24:40 +0200

    3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call calculateNumber', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
+  });
+});
